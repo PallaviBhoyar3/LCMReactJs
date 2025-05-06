25d939f2cc118e23710380cd3369107711f63d8e
pallavibhoyar <pallavi.prakshrao-bhoyar@capgemini.com>
2025-05-06 13:57:59 +0530

    add courses section

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -109,7 +109,7 @@ const Login = () => {
                   name="name"
                   value={signupInput.name}
                   onChange={(e) => changeInputHandler(e, "signup")}
-                  placeholder="Eg. patel"
+                  placeholder="Eg. pallavi"
                   required="true"
                 />
               </div>
@@ -120,7 +120,7 @@ const Login = () => {
                   name="email"
                   value={signupInput.email}
                   onChange={(e) => changeInputHandler(e, "signup")}
-                  placeholder="Eg. patel@gmail.com"
+                  placeholder="Eg. pallavi@gmail.com"
                   required="true"
                 />
               </div>
@@ -131,7 +131,7 @@ const Login = () => {
                   name="password"
                   value={signupInput.password}
                   onChange={(e) => changeInputHandler(e, "signup")}
-                  placeholder="Eg. xyz"
+                  placeholder="Eg. ***"
                   required="true"
                 />
               </div>
@@ -169,7 +169,7 @@ const Login = () => {
                   name="email"
                   value={loginInput.email}
                   onChange={(e) => changeInputHandler(e, "login")}
-                  placeholder="Eg. patel@gmail.com"
+                  placeholder="Eg. pallavi@gmail.com"
                   required="true"
                 />
               </div>
@@ -180,7 +180,7 @@ const Login = () => {
                   name="password"
                   value={loginInput.password}
                   onChange={(e) => changeInputHandler(e, "login")}
-                  placeholder="Eg. xyz"
+                  placeholder="Eg. pallavi"
                   required="true"
                 />
               </div>
--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -1,10 +1,130 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import React from "react";
+import React, { useState } from "react";
 import Course from "./Course";
 import { useGetPublishedCourseQuery } from "@/features/api/courseApi";
+
+// IMAGES
+import thumbnail1 from "../../assets/images/artificialThumbnail.png";
+import thumbnail2 from "../../assets/images/bootstrap.jpg";
+import thumbnail3 from "../../assets/images/iosAppThumbnail.jpg";
+import thumbnail4 from "../../assets/images/thumbnail3.png";
+import thumbnail5 from "../../assets/images/thumbnail5.png";
+import thumbnail6 from "../../assets/images/thumbnail6.png";
+import thumbnail7 from "../../assets/images/thumbnail7.png";
+import thumbnail8 from "../../assets/images/thumbnail8.png";
  
 const Courses = () => {
-  const {data, isLoading, isError} = useGetPublishedCourseQuery();
+  // const {data, isLoading, isError} = useGetPublishedCourseQuery();
+
+  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  console.log("isError",isError)
+
+  const data = [{
+    _id: "1",
+    courseTitle: "Introduction to Artificial Intelligence (AI)",
+    description: "This course is part of multiple programs.",
+    creator:{
+      name: "Rav Ahuja",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail1,
+    coursePrice: 1000,
+    courseLevel: 4.5,
+    totalEnrolledStudents: 50,
+  },
+  {
+    _id: "2",
+    courseTitle: "IOS App Development Basic", 
+    description: "This course is part of multiple programs.",
+    creator:{
+      name: "Parham Aarabi",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail2,
+    coursePrice: 2000,
+    courseLevel: 4.0,
+    totalEnrolledStudents: 30,
+  },
+  {
+    _id: "3",
+    courseTitle: "Javascript Basics",
+    description: "Description for Course 3",
+    creator:{
+      name: "Parham Aarabi",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail3,
+    coursePrice: 3000,
+    courseLevel: 4.0,
+    totalEnrolledStudents: 30,
+  },
+  {
+    _id: "4",
+    courseTitle: "React Js Basics",
+    description: "Description for Course 4",
+    creator:{
+      name: "Parham Aarabi",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail4,
+    coursePrice: 2000,
+    courseLevel: 4.0,
+    totalEnrolledStudents: 30,
+  },
+  {
+    _id: "5",
+    courseTitle: "HTML, CSS and Javascript",
+    description: "Description for Course 5",
+    creator:{
+      name: "Parham Aarabi",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail5,
+    coursePrice: 5000,
+    courseLevel: 4.5,
+    totalEnrolledStudents: 50,
+  },
+  {
+    _id: "6",
+    courseTitle: "React Native Advanced",
+    description: "Description for Course 6",
+    creator:{
+      name: "Parham Aarabi",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail6,
+    coursePrice: 6000,
+    courseLevel: 4.0,
+    totalEnrolledStudents: 30,
+  },
+  {
+    _id: "7",
+    courseTitle: "Developing Websites and Front-Ends with Bootstrap",
+    description: "Description for Course 7",
+    creator:{
+      name: "Parham Aarabi",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail7,
+    coursePrice: 7000,
+    courseLevel: 4.0,
+    totalEnrolledStudents: 30,
+  },
+  {
+    _id: "8",
+    courseTitle: "Moving to the Cloud",
+    description: "Moving to the Cloud",
+    creator:{
+      name: "Parham Aarabi",
+      // photoUrl: "https://via.placeholder.com/150",
+    },
+    courseThumbnail: thumbnail8,
+    coursePrice: 8000,
+    courseLevel: 4.0,
+    totalEnrolledStudents: 30,
+  }
+]
  
   if(isError) return <h1>Some error occurred while fetching courses.</h1>
 
@@ -18,7 +138,8 @@ const Courses = () => {
               <CourseSkeleton key={index} />
             ))
           ) : (
-           data?.courses && data.courses.map((course, index) => <Course key={index} course={course}/>) 
+          //  data?.courses && data.courses.map((course, index) => <Course key={index} course={course}/>) 
+          data && data.map((course, index) => <Course key={index} course={course}/>) 
           )}
         </div>
       </div>
